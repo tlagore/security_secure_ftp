6c38f4cf1eb176747d9f1a0efffd4e23a06d6a80
tyrone.lagore <tyrone.lagore@gfx03-sb.cs.ucalgary.ca>
2017-03-03 17:25:54 -0700

    added a check to ensure that iv and key is set before trying to encrypt/decrypt

--- client/aescs.py
+++ client/aescs.py
@@ -6,6 +6,9 @@ class AESCipher:
         self._bs = 16
         self._key = key
         self._iv = iv
+        self._ready = False
+        if self._iv and self._key:
+            self._ready = True
         
     def init_suites(self):
         print("init cipher suites: key = {0}, iv = {1}".format(self._key, self._iv))
@@ -14,23 +17,29 @@ class AESCipher:
             self._dcs = AES.new(self._key, AES.MODE_CBC, self._iv)
     
     def encrypt(self, plain_text):
-        b64_plain_text = base64.b64encode(plain_text)
+        ct = None
+        
+        if self._ready:
+            b64_plain_text = base64.b64encode(plain_text)
 
-        p_plain_text = self._pad(b64_plain_text)
+            p_plain_text = self._pad(b64_plain_text)
 
-        self._ecs = AES.new(self._key, AES.MODE_CBC, self._iv)
-        ct = self._ecs.encrypt(p_plain_text)
+            self._ecs = AES.new(self._key, AES.MODE_CBC, self._iv)
+            ct = self._ecs.encrypt(p_plain_text)
         
         return ct
 
     def decrypt(self, cipher_text):
-        self._dcs = AES.new(self._key, AES.MODE_CBC, self._iv)
-        dct = self._dcs.decrypt(cipher_text)
+        rb64t = None
 
-        upt = self._unpad(dct)
+        if self._ready:
+            self._dcs = AES.new(self._key, AES.MODE_CBC, self._iv)
+            dct = self._dcs.decrypt(cipher_text)
+            
+            upt = self._unpad(dct)
+            
+            rb64t = base64.b64decode(upt)
         
-        rb64t = base64.b64decode(upt)
-
         return rb64t
 
     def _pad(self, s):
@@ -44,6 +53,10 @@ class AESCipher:
 
     def set_key(self, key):
         self._key = key
+        if self._key and self._iv:
+            self._ready = True
 
     def set_iv(self, iv):
         self._iv = iv
+        if self._key and self._iv:
+            self._ready = True
